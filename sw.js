4e7845a0563dcad9f79b859a7d902edf3d6cb7d4
watanabe345-maker <watanabe345@gmail.com>
2026-09-10 06:29:15 +0900

    Keep rectangle QuickShape axis aligned

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'inknote-cache-v8';
+const CACHE_NAME = 'inknote-cache-v9';
 const CACHE_PREFIX = 'inknote-cache-';
 const APP_SHELL = [
   new URL('./', self.location.href).href,
