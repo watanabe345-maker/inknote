ee513fabc61338ad34ca4b6a9ed3a38f267f5c7d
watanabe345-maker <watanabe345@gmail.com>
2026-09-10 04:23:48 +0900

    Enforce Android palm intent delay

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE_NAME = 'inknote-cache-v1';
+const CACHE_NAME = 'inknote-cache-v2';
 const CACHE_PREFIX = 'inknote-cache-';
 const APP_SHELL = [
   new URL('./', self.location.href).href,
